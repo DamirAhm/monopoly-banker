7298b41ba106076eb09cab075fc585e5a27718b6
Proger3228 <50708474+Proger3228@users.noreply.github.com>
2019-08-21 16:19:50 +0400

    with https icon

--- app.js
+++ app.js
@@ -69,6 +69,7 @@ app.post("/", (req, res) => {
     let newGame = new Game({
         _id: new mongoose.Types.ObjectId()
     });
+    console.log(newGame);
     newGame.save(err => {
         if (err) {
             res.json({error: err});
