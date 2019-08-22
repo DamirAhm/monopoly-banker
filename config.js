dad7d2e9fd9bbaf635eb77fe27372163b51c679b
Proger3228 <50708474+Proger3228@users.noreply.github.com>
2019-08-22 13:28:25 +0400

    with icos without errors

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     PORT: process.env.PORT || 3000,
-    MONGO_URL: process.env.MONGO_URI || "mongodb://damir_ahm:ufV-wQC-58x-bXm@ds211648.mlab.com:11648/heroku_k4ddpxgz"
+    MONGO_URL: process.env.MONGODB_URI || "mongodb://damir_ahm:ufV-wQC-58x-bXm@ds211648.mlab.com:11648/heroku_k4ddpxgz"
 };
