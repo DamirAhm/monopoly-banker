601a0864409cba00cdf21fc89cde14b1060b1d41
Proger3228 <50708474+Proger3228@users.noreply.github.com>
2019-08-22 13:25:26 +0400

    with icos without errors

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     PORT: process.env.PORT || 3000,
-    MONGO_URL: "mongodb://banker666:8NHK9PFuv7bJmmIa@ds211648.mlab.com:11648/heroku_k4ddpxgz"
+    MONGO_URL: process.env.MONGOLAB_URL || process.env.MONGO_URL || "mongodb://damir_ahm:ufV-wQC-58x-bXm@ds211648.mlab.com:11648/heroku_k4ddpxgz"
 };
