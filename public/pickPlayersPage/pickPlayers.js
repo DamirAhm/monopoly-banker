c0b462b719a579980a9fb1aed4854c78449e5489
Proger3228 <damir221003@gmail.com>
2020-05-21 23:36:13 +0400

    Copy game url on click

--- public/pickPlayersPage/pickPlayers.js
+++ public/pickPlayersPage/pickPlayers.js
@@ -1,54 +1,53 @@
-let playersCont = document.getElementById("players-cont");
+let playersCont = document.getElementById( "players-cont" );
 
 let ids = [];
 
-if (document.documentElement.clientWidth >= 540) {
-    document.getElementById("url").innerText = "Ссылка на игру - " + document.location.href;
-} else {
-    document.getElementById("url").innerText = "Id игры - " + document.location.pathname.split("/")[1];
-
-}
-
-let newPlayer = (playerData) => {
-    let player = document.createElement("div");
-    player.classList.add("player");
-    player.classList.add("center");
-    ids.push(playerData._id);
-    // link.href = `${document.location.href}?playerId=${playerData.id}`;
+let newPlayer = ( playerData ) => {
+    let player = document.createElement( "div" );
+    player.classList.add( "player" );
+    player.classList.add( "center" );
+    ids.push( playerData._id );
     player.innerText = playerData.name;
-    player.addEventListener("click", (e) => {
-        onPlayerPick(e);
-    });
-    playersCont.appendChild(player);
+    player.addEventListener( "click", ( e ) => {
+        onPlayerPick( e );
+    } );
+    playersCont.appendChild( player );
 };
 
-if (playersCont) {
-    axios.get(`${document.location.href}players`).then(res => {
-        res.data.forEach(player => {
-            newPlayer(player);
-        });
-    });
+if ( playersCont ) {
+    axios.get( `${document.location.href}players` ).then( res => {
+        res.data.forEach( player => {
+            if ( !player.isPicked ) {
+                newPlayer( player );
+            }
+        } );
+    } );
 }
 
-let onPlayerPick = (e) => {
+let onPlayerPick = ( e ) => {
     let id = null;
-    let players = document.getElementsByClassName("player");
-    for (let i = 0; i < players.length; i++) {
-        if(players[i] === e.target){
-            id = ids[i];
+    let players = document.getElementsByClassName( "player" );
+    for ( let i = 0; i < players.length; i++ ) {
+        if ( players[ i ] === e.target ) {
+            id = ids[ i ];
         }
     }
-    axios.get(`${document.location.href}players?id=${id}`).then(res => {
-        if(!res.data.error) {
-            if(res.data.isPicked) {
-               e.preventDefault();
-               alert("This player is already picked");
-            }else{
-                document.location.replace(`?playerId=${id}`);
-                axios.get(`${document.location.href}pick-player?id=${id}`);
+    axios.get( `${document.location.href}players?id=${id}` ).then( res => {
+        if ( !res.data.error ) {
+            if ( res.data.isPicked ) {
+                e.preventDefault();
+                alert( "This player is already picked" );
+            } else {
+                document.location.replace( `?playerId=${id}` );
+                axios.get( `${document.location.href}pick-player?id=${id}` );
             }
-        }else{
-            alert(res.data.error);
+        } else {
+            alert( res.data.error );
         }
-    })
+    } )
 };
+
+const urlSpan = document.getElementById( "url" );
+urlSpan.addEventListener( "click", () => {
+    navigator.clipboard.writeText( document.location.href )
+} )
